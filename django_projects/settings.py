c1360d6b7fb33c60d991bb547231304f4b63d84e
animegazer <searchlie@gmail.com>
2021-07-20 21:07:09 +0900

    test

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -81,12 +81,14 @@ WSGI_APPLICATION = 'django_projects.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'name',
-        'USER': 'user',
-        'PASSWORD': '',
-        'HOST': 'host',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'name',
+        # 'USER': 'user',
+        # 'PASSWORD': '',
+        # 'HOST': 'host',
+        # 'PORT': '',
     }
 }
 
@@ -150,5 +152,6 @@ if not DEBUG:
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #追加
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
