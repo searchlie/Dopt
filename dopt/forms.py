cfacde6ab949d7b862aa29e7cc12566b5ce4e3ea
animegazer <searchlie@gmail.com>
2021-07-21 10:12:50 +0900

    formの微修正

--- dopt/forms.py
+++ dopt/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class UserForm(forms.Form):
     levels = forms.CharField(label='各因子の水準数', max_length=100)
-    number_of_experiments = forms.IntegerField(label='実験回数', max_value=1000, min_value=1)
-    number_of_trials = forms.IntegerField(label='計算回数', max_value=1000000, min_value=1, initial=1000)
+    number_of_experiments = forms.IntegerField(label='実施する実験回数', max_value=1000, min_value=1)
+    number_of_trials = forms.IntegerField(label='計算回数（探索する計画の数）', max_value=1000000, min_value=1, initial=1000)
     pass
